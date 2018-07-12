d02215b66cf1ab8326b51861bb43d26c1da0ab79
Fabio <faranpe@gmail.com>
2018-07-12 13:45:44 -0400

    First commit

---
+++ src/main/java/com/diplomado/qanodo/QanodoApplication.java
@@ -0,0 +1,12 @@
+package com.diplomado.qanodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QanodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QanodoApplication.class, args);
+	}
+}
